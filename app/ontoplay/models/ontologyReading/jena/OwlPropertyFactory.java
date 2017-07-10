79390b7e068995907064a426503ff65c6d693504
drozd <drozdowicz@gmail.com>
2017-07-10 22:14:40 +0200

    Squashed 'web/sxacml-admin/OntoPlay/' content from commit 8a3546d
    git-subtree-dir: web/sxacml-admin/OntoPlay
    git-subtree-split: 8a3546de5edbc722039e669f6554a1c9a7e051bc

---
+++ app/ontoplay/models/ontologyReading/jena/OwlPropertyFactory.java
@@ -0,0 +1,34 @@
+package ontoplay.models.ontologyReading.jena;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.jena.ontology.OntProperty;
+
+import ontoplay.models.InvalidConfigurationException;
+import ontoplay.models.ontologyModel.OntoProperty;
+
+public class OwlPropertyFactory {
+	private List<OwlPropertyFactory> factories = new ArrayList<OwlPropertyFactory>();
+	public void registerPropertyFactory(
+			OwlPropertyFactory datatypePropertyFactory) {
+		factories.add(datatypePropertyFactory);		
+	}
+	
+	public boolean canCreateProperty(OntProperty ontProperty){
+		for(OwlPropertyFactory fact : factories) {
+			if (fact.canCreateProperty(ontProperty)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public OntoProperty createProperty(OntProperty ontProperty){
+		for(OwlPropertyFactory fact : factories){
+			if(fact.canCreateProperty(ontProperty))
+				return fact.createProperty(ontProperty);
+		}
+		return null;
+	}
+}
